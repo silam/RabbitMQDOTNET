ae401b9805b86caf02fd33f7ae82490dcbdd4321
Si Lam <silam@hotmail.com>
2021-08-07 16:38:04 -0500

    Rabbitmq with .NET CORE

---
+++ RabbitMQConsumer/Program.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace RabbitMQConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create Factory
+            var factory = new ConnectionFactory
+            {
+                Uri = new Uri("amqp://guest:guest@localhost:5672")
+            };
+            // Create connection
+            using var connection = factory.CreateConnection();
+            // create a channel
+            using var channel = connection.CreateModel();
+
+            // declare a queue
+            channel.QueueDeclare("demo-queue", durable: true, exclusive: false,
+                autoDelete: false, arguments: null);
+
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            consumer.Received += (sender, e) =>
+            {
+                var body = e.Body.ToArray(); // this is byte array
+                var message = Encoding.UTF8.GetString(body);
+
+                Console.WriteLine(message);
+            };
+
+            channel.BasicConsume("demo-queue", true, consumer);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RabbitmqProducer/RabbitmqProducer/Program.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RabbitmqProducer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create Factory
+            var factory = new ConnectionFactory { 
+                Uri = new Uri("amqp://guest:guest@localhost:5672")};
+            // Create connection
+            using var connection = factory.CreateConnection();
+            // create a channel
+            using var channel = connection.CreateModel();
+
+            // declare a queue
+            channel.QueueDeclare("demo-queue", durable: true, exclusive: false,
+                autoDelete: false, arguments: null);
+
+            var message = new { Name = "Producer", message = "Hello!" };
+
+            var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+
+            channel.BasicPublish("" // exchange
+                                 , "demo-queue" // routing key : name of the queue
+                                  , null, // basic property
+                                  body              );
+
+        }
+    }
+}
